d32cc0fa0caca2093a4a6b670f6a0051ed0599a6
Nicolo Ballestriero <n.ballestriero@gmail.com>
2026-07-12 13:47:41 +0200

    test: add governed scaffolder integration test scaffold

---
+++ tools/MR-0002/tests/create-governed-implementation-scaffold.test.mjs
@@ -0,0 +1,10 @@
+/**
+ * @file Governed implementation scaffolder integration test.
+ *
+ * @implementsRequirement MR-0002ADR-0003REQ-0001GOV-0002
+ * @derivedFromDecision MR-0002/ADR-0003
+ * @macroRequirement MR-0002
+ * @implementationStatus scaffolded
+ *
+ * TODO: implement the governed test behavior.
+ */
