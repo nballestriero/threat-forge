a74675079685880a9e0e33fcf6b5234dc2ad1e60
Nicolo Ballestriero <n.ballestriero@gmail.com>
2026-07-24 12:54:41 +0200

    tooling: scaffold common analysis finding model

---
+++ tools/MR-0005/lib/common-analysis-finding-model.mjs
@@ -0,0 +1,10 @@
+/**
+ * @file Canonical common analysis finding model.
+ *
+ * @implementsRequirement MR-0005ADR-0002REQ-0001
+ * @derivedFromDecision MR-0005/ADR-0002
+ * @macroRequirement MR-0005
+ * @implementationStatus scaffolded
+ *
+ * TODO: implement the governed source-module behavior.
+ */
