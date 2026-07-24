17f220c2922e9310fd90ab61d1d5f576d4ae151d
Nicolo Ballestriero <n.ballestriero@gmail.com>
2026-07-24 15:16:19 +0200

    tooling: scaffold common analysis finding validator

---
+++ tools/MR-0005/check-common-analysis-findings.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/**
+ * @file Deterministic common analysis finding validator.
+ *
+ * @implementsRequirement MR-0005ADR-0002REQ-0001GOV-0001
+ * @derivedFromDecision MR-0005/ADR-0002
+ * @macroRequirement MR-0005
+ * @implementationStatus scaffolded
+ *
+ * TODO: implement the governed tool behavior.
+ */
