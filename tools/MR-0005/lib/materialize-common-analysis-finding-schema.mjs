839bd6be19efde21763557074771058e1d1f9885
Nicolo Ballestriero <n.ballestriero@gmail.com>
2026-07-25 10:04:10 +0200

    tooling: scaffold common analysis finding schema materializer

---
+++ tools/MR-0005/lib/materialize-common-analysis-finding-schema.mjs
@@ -0,0 +1,10 @@
+/**
+ * @file Common analysis finding editor schema materializer.
+ *
+ * @implementsRequirement MR-0005ADR-0002REQ-0001GOV-0001
+ * @derivedFromDecision MR-0005/ADR-0002
+ * @macroRequirement MR-0005
+ * @implementationStatus scaffolded
+ *
+ * TODO: implement the governed source-module behavior.
+ */
